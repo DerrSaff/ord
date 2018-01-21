6dc63af46d6e2af43cad2ce09977c16a171d9194
Andrew <andrewgladilin@gmail.com>
2018-01-21 23:56:39 +0300

    Fix QueryForm

--- imports/ui/QueryForm.js
+++ imports/ui/QueryForm.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const QueryForm = () => (
   <form method="get">
-    <input class="search_field" type="text" class=""/>
-    <input class="submit_button" type="submit"/>
+    <input className="search_field" type="text" />
+    <input className="submit_button" type="submit"/>
   </form>
 )
 
