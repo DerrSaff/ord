ed55c833dc982f0e3c148ab8a1028a92d1837bf5
Andrew <andrewgladilin@gmail.com>
2018-01-22 00:02:41 +0300

    Add App component

---
+++ imports/ui/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import Navigation from './Navigation'
+import QueryPage from './QueryPage'
+import DictPage from './DictPage'
+import About from './About'
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+      <hr/>
+
+      <Route exact path="/" component={QueryPage}/>
+      <Route path="/dictionary" component={DictPage}/>
+      <Route path="/about" component={About}/>
+    </div>
+  </Router>
+)
+
+export default App
