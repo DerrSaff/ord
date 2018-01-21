8b9b5f0ea238b835c6926eba6133fd22574edb7a
Andrew <andrewgladilin@gmail.com>
2018-01-21 22:25:25 +0300

    Add QueryForm component

---
+++ imports/ui/QueryForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const QueryForm = () => (
+  <form method="get">
+    <input class="search_field" type="text" class=""/>
+    <input class="submit_button" type="submit"/>
+  </form>
+)
+
+export default QueryForm
