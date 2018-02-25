963a30f0087467a4bcb3a7b57bf8f624e27fb55c
Andrew <andrewgladilin@gmail.com>
2018-02-25 22:04:06 +0300

    Add styles

--- imports/ui/EntityForm.js
+++ imports/ui/EntityForm.js
@@ -1,41 +1,53 @@
 import React from 'react'
 
-const EntityForm = () => (
-  <div>
-    <h1>
-      action
-    </h1>
+class EntityForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      "action": "create"
+    }
+  }
 
-    <div>
-      <label>Select type:</label>
-      <select>
-        <option>Word</option>
-        <option>Phrase</option>
-      </select>
-    </div>
+  render() {
+    return (
+      <div className="entity-form">
+        <h1>
+          {this.state.action}
+        </h1>
 
-    <div>
-      <label>Spelling:</label>
-      <input type="text"/>
-    </div>
+        <div className="input-group">
+          <label>Select type:</label>
+          <select>
+            <option>Word</option>
+            <option>Phrase</option>
+          </select>
+        </div>
 
-    <h3>Common params</h3>
+        <div className="input-group">
+          <label>Spelling:</label>
+          <input type="text"/>
+        </div>
 
-    <div>
-      <input type="text"/>: 
-      <input type="text"/>
-    </div>
-    <div>
-      <input type="text"/>: 
-      <input type="text"/>
-    </div>
-    <div>
-      <input type="text"/>: 
-      <input type="text"/>
-    </div>
+        <h3>Common params</h3>
 
-    <h3>Semantics</h3>
-  </div>
-)
+        <div className="input-group">
+          <input type="text"/>:
+          <input type="text"/>
+        </div>
+        <div className="input-group">
+          <input type="text"/>:
+          <input type="text"/>
+        </div>
+        <div className="input-group">
+          <input type="text"/>:
+          <input type="text"/>
+        </div>
+
+        <h3>Semantics</h3>
+      </div>
+
+    )
+  }
+}
 
 export default EntityForm
