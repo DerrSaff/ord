5a33ef08b7a8e02b5dae1ec42fe75b42bdb36090
Andrew <andrewgladilin@gmail.com>
2018-03-01 11:35:21 +0300

    Add simple language definer

--- client/main.js
+++ client/main.js
@@ -3,11 +3,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from '../imports/ui/App';
-const { I18n } = require('react-i18nify');
+const {I18n} = require('react-i18nify');
 import translations from "../imports/i18n/i18n"
 
-Meteor.Meteor.startup(() => {
-  ReactDOM.render(<App/>, document.getElementById('app'))
-  I18n.setTranslations(translations);
-  I18n.setLocale('en');
-})
+Meteor.Meteor
+  .startup(() => {
+    ReactDOM.render(
+      <App/>, document.getElementById('app'))
+    I18n.setTranslations(translations);
+
+    if (navigator.language.substr(0, 2) === "en") {
+      I18n.setLocale('en');
+    } else if (navigator.language.substr(0, 2) === "ru") {
+      I18n.setLocale('ru');
+    } else {
+      I18n.setLocale('en');
+    }
+  })
