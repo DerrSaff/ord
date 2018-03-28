005fcda8b47c8c58735c2dc408304cda51b5225e
Andrew <andrewgladilin@gmail.com>
2018-03-28 23:15:21 +0300

    Add Clear text button

--- imports/ui/QueryForm.js
+++ imports/ui/QueryForm.js
@@ -1,11 +1,33 @@
 import React from 'react'
 
-const QueryForm = () => (
-  <div className="search_form">
-    <input className="search_field" type="text" />
-    <button>Clear</button>
-    <button>Submit</button>
-  </div>
-)
+class QueryForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClickClear = this.handleClickClear.bind(this);
+  }
+  handleClickClear() {
+    console.log(this.textInput)
+    this.textInput.value = ''
+    this.textInput.focus();
+  }
+  render() {
+    return (
+      <div className="search_form">
+        <input
+          className="search_field"
+          type="text"
+          ref={(input) => { this.textInput = input; }}
+        />
+
+        <button
+          onClick={this.handleClickClear}>
+          Clear
+        </button>
+
+        <button>Submit</button>
+      </div>
+    )
+  }
+}
 
 export default QueryForm
